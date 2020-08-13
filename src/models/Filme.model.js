81040e5b7d61adbb32a4fb5d2de546803a2d6dd8
Rodrigo Russo <rodrigo.esteves@webjump.com.br>
2020-08-12 22:55:49 -0300

    WJ-02- Criar Filmes Model;

---
+++ src/models/Filme.model.js
@@ -0,0 +1,34 @@
+/**
+ * Filme model
+ */
+export class Filme {
+    /**
+     * Model constructor
+     * @param {string} title 
+     * @param {string} posterPath 
+     * @param {string} [backdropPath] 
+     * @param {boolean} [favorite] 
+     */
+    constructor(title, posterPath, backdropPath, favorite = false) {
+        this._title = title;
+        this._posterPath = posterPath;
+        this._backdropPath = backdropPath;
+        this._favorite = favorite;
+    }
+    toggleFavorite() {
+        this._favorite = !this._favorite;
+    }
+    // Getters
+    get title() {
+        return this._title;
+    }
+    get posterPath() {
+        return this._posterPath;
+    }
+    get backdropPath() {
+        return this._backdropPath;
+    }
+    get favorite() {
+        return this._favorite;
+    }
+}
